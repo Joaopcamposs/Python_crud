6e99c0bd8be9940ce57815a63a0982a06e889525
Joaopcamposs <joaopcamposs@outlook.com>
2022-04-10 23:25:02 -0300

    CRUD e API completos e funcional

---
+++ database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+# import config #config de variavel de ambiente
+
+# DATABASE_URL = config.DATABASE_URL  #variavel de ambiente
+DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/projetoWeb"
+#root é o usuário
+#1234 é a senha
+#3306 é a porta
+#iftm é o schema
+
+db_engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
+
+Base = declarative_base()
+
+def get_db():
+    """
+    Function to generate db session
+    :return: Session
+    """
+    db = None
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
